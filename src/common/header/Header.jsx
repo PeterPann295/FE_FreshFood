b39c2a57ab7c2842943ba943bd0f7cea1507df67
PeterPann295 <21130354@st.hcmuaf.edu.vn>
2024-09-23 21:49:27 +0700

    update

---
+++ src/common/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './header.css';
+const Header = () => {
+    return (
+        <>
+        <header className="fixed-top p-3 text-bg-success" style={{paddingTop:'100px'}}>
+
+        </header>
+        </>
+    )
+}
